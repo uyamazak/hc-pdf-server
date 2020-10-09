af663f329f24ec8fd59f114e37231e1d54a10070
uyamazak <yu.yamazaki85@gmail.com>
2020-10-09 19:40:14 +0900

    pdf response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header } from '@nestjs/common'
+import { Controller, Get, Header, Res } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
@@ -7,7 +7,20 @@ export class AppController {
 
   @Get()
   @Header('Content-Type', 'application/pdf')
-  async getHello(): Promise<string> {
-    return  (await this.appService.get('https://www.google.com')).toString()
+  async getHello(@Res() res): Promise<void> {
+    const buffer = await this.appService.get('https://www.google.com')
+    res.set({
+      // pdf
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename=invoice.pdf',
+      'Content-Length': buffer.length,
+
+      // prevent cache
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': 0,
+    })
+
+    res.end(buffer)
     }
 }
