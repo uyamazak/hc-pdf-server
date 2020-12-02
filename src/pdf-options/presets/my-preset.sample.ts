4af8f3cc4629a928d070a8bc102442f151cc042c
uyamazak <yu.yamazaki85@gmail.com>
2020-12-02 18:32:49 +0900

    to fit type, change format name to lowercase

--- src/pdf-options/presets/my-preset.sample.ts
+++ src/pdf-options/presets/my-preset.sample.ts
@@ -2,11 +2,11 @@
  * @see https://github.com/puppeteer/puppeteer/blob/v5.3.1/docs/api.md#pagepdfoptions
  */
 
-import { PDFOptions } from 'puppeteer'
+import { PDFOptions } from 'puppeteer/lib/cjs/puppeteer/common/PDFOptions'
 
 export const PDFOptionsPreset: { [key: string]: PDFOptions } = {
   MYA4: {
-    format: 'A4',
+    format: 'a4',
   },
   /**
    * add your options
